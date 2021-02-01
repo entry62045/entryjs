c6ef154ea8b8b955db997f8667d798373fe922e9
62045 <77736636+entry62045@users.noreply.github.com>
2021-02-01 16:02:41 +0900

    Create macro.js

---
+++ macro.js
@@ -0,0 +1,13 @@
+function start() {
+  $('.editDescriptionBtn').trigger('click');
+  $('.editConfirmBtn').trigger('click');
+  console.log('작품이 업데이트되었습니다');
+}
+var macrotime = 0;
+var setmacrotime = 0;
+macrotime = prompt("매크로 사용 시간 입력(취소 시 0 입력)");
+setmacrotime = time * 1000;
+if (setmacrotime != 0) {
+  console.log('매크로를 시작합니다.')
+  setInterval(start, setmacrotime)
+}
