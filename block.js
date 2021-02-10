092ddd8919b286b28d56c61e280cff2f938cc2cb
entry62045 <solchan2007@gmail.com>
2021-02-10 15:01:33 +0900

    주소 변경

--- block.js
+++ block.js
@@ -1,2 +1,2 @@
-console.log('특급 블럭 링크가 이동되었습니다.\nhttps://raw.githack.com/entry62045/expressblock/main/block.js\n자동으로 변경된 내용으로 로드합니다.');
+console.log('특급 블럭 링크가 이동되었습니다.\nhttps://raw.githack.com/entry62045/expressblock/main/block.js\n자동으로 변경된 주소로 로드합니다.');
 import('https://raw.githack.com/entry62045/expressblock/main/block.js');
